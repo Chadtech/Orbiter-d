495da2195fdf35b4bc5a5f1242d9cb8f397463f6
Chadtech <chadstearns1@gmail.com>
2016-08-15 19:29:19 -0400

    Renamed updateObjects to HandlePhysics

---
+++ src/elm/update/HandlePhysics.elm
@@ -0,0 +1,40 @@
+module HandlePhysics exposing (handlePhysics)
+
+import Types       exposing (..)
+import SpaceObject exposing (SpaceObject, SpaceObjects)
+import Dict        exposing (fromList, values, Dict)
+import List        exposing (map)
+import SetPosition exposing (setPosition)
+import SetThrust   exposing (setThrust)
+import SetGravity  exposing (setGravity)
+import SetWeight   exposing (setWeight)
+import Consumption exposing (consumeAir, consumeFuel)
+import Game        exposing (Model)
+
+handlePhysics : Time -> Model -> Model
+handlePhysics dt model =
+  let {localObjects, remoteObjects} = model in
+  { model
+  | localObjects = applyPhysics dt localObjects
+  , remoteObjects = applyPhysics dt remoteObjects
+  }
+
+applyPhysics : Time -> Dict String SpaceObject -> Dict String SpaceObject
+applyPhysics dt objects =
+  values objects
+  |>map (physics dt)
+  |>map pairWithuuid
+  |>fromList
+
+physics : Time -> SpaceObject -> SpaceObject
+physics dt =
+  consumeAir dt
+  >>consumeFuel dt
+  >>setWeight
+  >>setThrust
+  >>setGravity dt
+  >>setPosition dt
+
+pairWithuuid : SpaceObject -> (String, SpaceObject)
+pairWithuuid object =
+  (object.uuid, object)
