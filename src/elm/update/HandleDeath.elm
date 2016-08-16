8f404380bcbca412d0ebe9d9240b02956d7bf6bc
Chadtech <chadstearns1@gmail.com>
2016-08-16 16:50:15 -0400

    Put high speed collision into its own module

--- src/elm/update/HandleDeath.elm
+++ src/elm/update/HandleDeath.elm
@@ -6,6 +6,7 @@ import SpaceObject exposing (..)
 import Dict exposing (..)
 import Maybe exposing (withDefault)
 import FellIntoPlanet exposing (fellIntoPlanet)
+import HighSpeedCollision exposing (highSpeedCollision)
 import List exposing (head)
 
 handleDeath : Model -> Model
@@ -24,42 +25,8 @@ handleDeath model =
       fellIntoPlanet model deathMessage
 
     HighSpeedCollision ->
-      let
-        player =
-          get model.playerId model.localObjects
-          |>withDefault dummyShip
-          |>\p -> 
-            { p 
-            | sprite =
-              { src        = "ship/ship-exploded"
-              , dimensions = (47, 47)
-              , area       = (138, 138)
-              , position   = (0,0)
-              }
-            , engine = 
-              { boost = False
-              , thrusters = []
-              }
-            }
+      highSpeedCollision model deathMessage
 
-        newPlayerId = 
-          model.localObjects
-          |>toList
-          |>head
-          |>withDefault (dummyShip.uuid, dummyShip)
-          |>snd
-          |> (.uuid)
-      in
-      { model
-      | localObjects =
-          let {playerId, localObjects} = model in
-          insert 
-            playerId 
-            player
-            localObjects
-      , died = True
-      , deathMessage = deathMessage
-      }
     _ -> model
 
 
---
+++ src/elm/update/HighSpeedCollision.elm
@@ -0,0 +1,47 @@
+module HighSpeedCollision exposing (highSpeedCollision)
+
+import Game exposing (..)
+import Types exposing (..)
+import SpaceObject exposing (..)
+import Dict exposing (..)
+import Maybe exposing (withDefault)
+import List exposing (head)
+
+highSpeedCollision : Model -> String -> Model
+highSpeedCollision model deathMessage =
+  let
+    player =
+      get model.playerId model.localObjects
+      |>withDefault dummyShip
+      |>\p -> 
+        { p 
+        | sprite =
+          { src        = "ship/ship-exploded"
+          , dimensions = (47, 47)
+          , area       = (138, 138)
+          , position   = (0,0)
+          }
+        , engine = 
+          { boost = False
+          , thrusters = []
+          }
+        }
+
+    newPlayerId = 
+      model.localObjects
+      |>toList
+      |>head
+      |>withDefault (dummyShip.uuid, dummyShip)
+      |>snd
+      |> (.uuid)
+  in
+  { model
+  | localObjects =
+      let {playerId, localObjects} = model in
+      insert 
+        playerId 
+        player
+        localObjects
+  , died = True
+  , deathMessage = deathMessage
+  }
